f9fecb0ba6ffda475f3a47b2ed90224742f52af5
Eiji-16 <ajph305@gmail.com>
2026-07-16 16:20:16 +0800

    Final

--- Aula F75 model/Keebs.py
+++ Aula F75 model/Keebs.py
@@ -602,3 +602,51 @@
 	bpy.context. = False
 	bpy.context. = True
 	bpy.context. = False
+	bpy.context. = False
+	bpy.ops.outliner.item_activate(deselect_all=True)
+	bpy.ops.outliner.delete()
+	bpy.ops.object.editmode_toggle()
+	bpy.ops.object.camera_add(enter_editmode=False, align='VIEW', location=(0, 0, 0), rotation=(1.07512, 6.67394e-07, -0.495675), scale=(1, 1, 1))
+	[obj.select_set(False) for obj in bpy.context.view_layer.objects.selected.values()]
+	[bpy.context.view_layer.objects.get(obj).select_set(True) for obj in ['Camera']]
+	bpy.context.view_layer.objects.active = bpy.data.objects['Camera']
+	bpy.ops.object.delete(use_global=False, confirm=False)
+	bpy.ops.object.delete(use_global=False, confirm=False)
+	bpy.ops.object.camera_add(enter_editmode=False, align='VIEW', location=(0, 0, 0), rotation=(1.09607, 1.86511e-07, -0.551526), scale=(1, 1, 1))
+	[obj.select_set(False) for obj in bpy.context.view_layer.objects.selected.values()]
+	[bpy.context.view_layer.objects.get(obj).select_set(True) for obj in ['Camera']]
+	bpy.context.view_layer.objects.active = bpy.data.objects['Camera']
+	bpy.ops.outliner.item_activate(deselect_all=True)
+	[obj.select_set(False) for obj in bpy.context.view_layer.objects.selected.values()]
+	[bpy.context.view_layer.objects.get(obj).select_set(True) for obj in ['Camera']]
+	bpy.context.view_layer.objects.active = bpy.data.objects['Camera']
+	bpy.ops.transform.translate(value=(-1.66904, -1.04409, 0.898116), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', mirror=False, use_proportional_edit=False, proportional_edit_falloff='SPHERE', proportional_size=9.84974, use_proportional_connected=False, use_proportional_projected=False, snap=False, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=True, use_snap_edit=True, use_snap_nonedit=True, use_snap_selectable=False)
+	bpy.ops.transform.translate(value=(-0, -1.09715, -0), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, True, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SPHERE', proportional_size=9.84974, use_proportional_connected=False, use_proportional_projected=False, snap=False, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=True, use_snap_edit=True, use_snap_nonedit=True, use_snap_selectable=False)
+	bpy.ops.transform.translate(value=(0, 0, 0.170951), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, False, True), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SPHERE', proportional_size=9.84974, use_proportional_connected=False, use_proportional_projected=False, snap=False, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=True, use_snap_edit=True, use_snap_nonedit=True, use_snap_selectable=False)
+	bpy.ops.outliner.item_activate(deselect_all=True)
+	[obj.select_set(False) for obj in bpy.context.view_layer.objects.selected.values()]
+	[bpy.context.view_layer.objects.get(obj).select_set(True) for obj in ['Camera']]
+	bpy.context.view_layer.objects.active = bpy.data.objects['Camera']
+	bpy.ops.transform.translate(value=(0.138363, -0.0399678, 0.100546), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', mirror=False, use_proportional_edit=False, proportional_edit_falloff='SPHERE', proportional_size=9.84974, use_proportional_connected=False, use_proportional_projected=False, snap=False, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=True, use_snap_edit=True, use_snap_nonedit=True, use_snap_selectable=False)
+	bpy.ops.transform.translate(value=(0, 0.453867, 0), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, True, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SPHERE', proportional_size=9.84974, use_proportional_connected=False, use_proportional_projected=False, snap=False, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=True, use_snap_edit=True, use_snap_nonedit=True, use_snap_selectable=False)
+	bpy.ops.transform.translate(value=(0.213753, 0, 0), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(True, False, False), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SPHERE', proportional_size=9.84974, use_proportional_connected=False, use_proportional_projected=False, snap=False, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=True, use_snap_edit=True, use_snap_nonedit=True, use_snap_selectable=False)
+	bpy.ops.transform.translate(value=(-0, -0, -0.213837), orient_type='GLOBAL', orient_matrix=((1, 0, 0), (0, 1, 0), (0, 0, 1)), orient_matrix_type='GLOBAL', constraint_axis=(False, False, True), mirror=False, use_proportional_edit=False, proportional_edit_falloff='SPHERE', proportional_size=9.84974, use_proportional_connected=False, use_proportional_projected=False, snap=False, snap_elements={'INCREMENT'}, use_snap_project=False, snap_target='CLOSEST', use_snap_self=True, use_snap_edit=True, use_snap_nonedit=True, use_snap_selectable=False)
+	bpy.ops.outliner.item_activate(deselect_all=True)
+	bpy.context. = True
+	[obj.select_set(False) for obj in bpy.context.view_layer.objects.selected.values()]
+	[bpy.context.view_layer.objects.get(obj).select_set(True) for obj in []]
+	bpy.context.view_layer.objects.active = bpy.data.objects['Camera']
+	bpy.context.region.active_panel_category = 'Item'
+	bpy.context.region.active_panel_category = 'Tool'
+	bpy.context.region.active_panel_category = 'View'
+	bpy.context.region.active_panel_category = 'View'
+	bpy.ops.outliner.item_activate(deselect_all=True)
+	[obj.select_set(False) for obj in bpy.context.view_layer.objects.selected.values()]
+	[bpy.context.view_layer.objects.get(obj).select_set(True) for obj in ['Caps']]
+	bpy.context.view_layer.objects.active = bpy.data.objects['Caps']
+	bpy.data.materials["Material.001"].node_tree.nodes["Principled BSDF"].inputs[2].default_value = 1
+	bpy.data.materials["Material.001"].node_tree.nodes["Principled BSDF"].inputs[2].default_value = 0.791667
+	bpy.data.materials["Material.001"].node_tree.nodes["Principled BSDF"].inputs[1].default_value = 0
+	[obj.select_set(False) for obj in bpy.context.view_layer.objects.selected.values()]
+	[bpy.context.view_layer.objects.get(obj).select_set(True) for obj in ['Case']]
+	bpy.context.view_layer.objects.active = bpy.data.objects['Case']
